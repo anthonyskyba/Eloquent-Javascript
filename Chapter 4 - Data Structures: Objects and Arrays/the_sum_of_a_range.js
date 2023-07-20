2cdcf7c561b0f9e353a582174630efdec2a4006e
Anthony Skyba <122836976+anthonyskyba@users.noreply.github.com>
2023-07-19 22:00:26 -0400

    Update the_sum_of_a_range.js

--- Chapter 4 - Data Structures: Objects and Arrays/the_sum_of_a_range.js
+++ Chapter 4 - Data Structures: Objects and Arrays/the_sum_of_a_range.js
@@ -1,1 +1,32 @@
+const range = (num1, num2, scale = 1) => {
+    let output = [num1]
+    if (scale == 0)
+      scale = 1
+    if (num1 < num2) {
+      for (num1; num1 < num2; num1 += scale){
+        output.push(num1 + scale)
+      }
+    } else {
+      if (scale > -1)
+        scale = -scale
+      for (num2; num1 > num2; num1 += scale){
+        output.push(num1 + scale)
+      }
+    }
+    return output
+  }
 
+  const sum = array => {
+    let output = 0
+    for (let counter = 0; counter < array.length; counter++) {
+      output += array[counter]
+    }
+    return output
+  }
+  
+  console.log(range(1, 10))
+  // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  console.log(range(5, 2, -1))
+  // → [5, 4, 3, 2]
+  console.log(sum(range(1, 10)));
+  // → 55
