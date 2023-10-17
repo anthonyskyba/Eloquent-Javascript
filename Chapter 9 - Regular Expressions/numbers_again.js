40146d61d00754d3703d7b46134ff7e4345fe377
Anthony Skyba <122836976+anthonyskyba@users.noreply.github.com>
2023-10-17 18:51:44 -0400

    Update numbers_again.js

--- Chapter 9 - Regular Expressions/numbers_again.js
+++ Chapter 9 - Regular Expressions/numbers_again.js
@@ -1,6 +1,6 @@
 // Fill in this regular expression.
 // not done (haven't seen solution) will come back later to fix this. Issue is that it works for matching numbers but not all of the string has to match for it to pass the test
-let number = /((^\+|^-)(\d+(\.\d*)?|\.\d+)(e((\+|-)\d+|\d+))?)|(^\.\d+(e((\+|-)\d+|\d+))?)|(^\d+(\.\d*)?(e((\+|-)\d+|\d+))?)/;
+let number = /^(\+|-)?(\d+\.$|(\d+(\.\d+)?|\.\d+)(e((\+|-)\d+|\d+))?\b$)/i;
 
 // Tests:
 for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
